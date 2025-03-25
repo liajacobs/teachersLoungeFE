b0a2b5d308e058870f7c15c391cd6cb12d8a628c
Lia Jacobs <jacobs.1236@osu.edu>
2025-03-24 22:03:15 -0400

    Fix new chat

--- src/View/UserView/MessageView/CreateNewChatView.js
+++ src/View/UserView/MessageView/CreateNewChatView.js
@@ -29,35 +29,33 @@ function CreateNewChatView({ navigation }) {
     fetchUsers();
   }, [isFocused]);
   return (listOfUsers.length > 0) ? (
-    <View style={App_StyleSheet.listings}>
-      <SafeArea>
-        <FlatList
-          data={listOfUsers}
-          renderItem={({ item }) => (
-            <UserListing
-              user={item}
-              onClick={async () => {
-                const created = await createConversation(route.params.User.userUserName, item.email);
-                if (created) {
-                  Alert.alert("Success", "Conversation created");
-                }else{
-                  Alert.alert("Error", "Conversation already exists!");
-                }
-              }}
-            />
-          )}
-        />
-      </SafeArea>
-    </View>
-  )
-  : 
-  (<View style={App_StyleSheet.listings}>
     <SafeArea>
-      <Text style={App_StyleSheet.moderation_view}>
-      You need to have at least 1 friend in order to create a chat.
-      </Text>
+      <FlatList
+        data={listOfUsers}
+        renderItem={({ item }) => (
+          <UserListing
+            user={item}
+            onClick={async () => {
+              const created = await createConversation(route.params.User.userUserName, item.email);
+              if (created) {
+                Alert.alert("Success", "Conversation created");
+              } else {
+                Alert.alert("Error", "Conversation already exists!");
+              }
+            }}
+          />
+        )}
+      />
     </SafeArea>
-  </View>);
+  )
+    :
+    (<View style={App_StyleSheet.listings}>
+      <SafeArea>
+        <Text style={App_StyleSheet.moderation_view}>
+          You need to have at least 1 friend in order to create a chat.
+        </Text>
+      </SafeArea>
+    </View>);
 }
 
 export default CreateNewChatView;
